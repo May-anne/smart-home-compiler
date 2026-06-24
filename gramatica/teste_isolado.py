6780a7f59d59cc6db894762deedd86e20f8967c0
Arthur Lima <arthurlm@protonmail.com>
2026-06-24 19:44:51 -0300

    abububle

--- gramatica/teste_isolado.py
+++
@@ -1,6 +0,0 @@
-from gramatica import GRAMATICA_COMPLETA
-
-linhas = GRAMATICA_COMPLETA.split("\n")
-for i, linha in enumerate(linhas, start=1):
-    if 58 <= i <= 68:
-        print(f"{i}: {linha}")
